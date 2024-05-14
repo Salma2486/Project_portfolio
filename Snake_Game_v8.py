1b8ce4cbb9488ca2b1906c3b1445cbf3eff44772
Salma2486 <138704540+Salma2486@users.noreply.github.com>
2024-05-14 23:29:54 +0000

    u

--- Snake_Game_v8.py
+++ Snake_Game_v8.py
@@ -1,11 +1,12 @@
-import pygame, sys, random
+import pygame, sys, random, json
 from pygame.math import Vector2
 
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-        self.direction = Vector2(1, 0)
+        self.direction = Vector2(0, 0)
         self.new_block = False
+        self.start_moving = False
         
 
         self.head_up = pygame.image.load("head_up.png").convert_alpha()
@@ -47,8 +48,9 @@ class SNAKE:
 
 
         self.crunch_sound = pygame.mixer.Sound('crunch_2.mp3')
-        self.head = self.head_left
-        self.tail = self.tail_right
+        self.explosion = pygame.mixer.Sound('bomb2.mp3')
+        self.head = self.head_right
+        self.tail = self.tail_left
     
 
     def draw_snake(self):
@@ -115,10 +117,14 @@ class SNAKE:
 
     def reset(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-        self.direction = Vector2(1, 0)
+        self.direction = Vector2(0, 0)
+        
 
     def play_crunch_sound(self):
         self.crunch_sound.play()
+
+    def play_explosion(self):
+        self.explosion.play()
 
 class FRUIT:
     def __init__(self):
@@ -127,7 +133,19 @@ class FRUIT:
     def draw_fruit(self):
         fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
         screen.blit(apple, fruit_rect)
-        #pygame.draw.rect(screen, (255, 0, 0), fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0 , cell_number - 1)
+        self.y = random.randint(0 , cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+class BOMB:
+    def __init__(self):
+        self.randomize()
+
+    def draw_bomb(self):
+        bomb_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        screen.blit(bomb, bomb_rect)
 
     def randomize(self):
         self.x = random.randint(0 , cell_number - 1)
@@ -139,8 +157,19 @@ class MAIN:
         self.snake = SNAKE()
         self.fruit = FRUIT()
         self.game_state = "start_menu"
+        self.high_score = self.load_high_score()
+        self.bomb = BOMB()
+        self.game_over_sound_played = False
+        pygame.mixer.init()
+        self.background_music = pygame.mixer.Sound('gamesound.mp3')
+        self.background_music.play(-1)
+        
 
     def update(self):
+        if self.game_state != "playing":
+            return
+        if self.snake.start_moving == False:
+            return
         self.snake.move_snake()
         self.check_collision()
         self.check_fail()
@@ -150,16 +179,33 @@ class MAIN:
         self.fruit.draw_fruit()
         self.snake.draw_snake()
         self.draw_score()
+        self.bomb.draw_bomb()
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
             self.snake.play_crunch_sound()
+            
+
+
+        if self.bomb is not None and self.bomb.pos == self.snake.body[0]:
+            
+            if len(self.snake.body) > 3:
+                self.snake.body.pop()
+                self.snake.play_explosion()
+                self.bomb = BOMB()
+            else:
+                self.game_over()
+                return
 
         for block in self.snake.body[1:]:
             if block == self.fruit.pos:
                 self.fruit.randomize()
+            if self.bomb is not None and block == self.bomb.pos:
+                self.bomb.randomize()
+
+        
 
     def check_fail(self):
         if self.game_state == "game_over":
@@ -172,9 +218,16 @@ class MAIN:
                 if self.game_state == "playing":
                     self.game_over()
 
+        current_score = len(self.snake.body) - 3
+        if current_score > self.high_score:
+            self.high_score = current_score
+            self.save_high_score()
+
     def game_over(self):
         self.snake.reset()
         self.game_state = "game_over"
+        self.game_over_sound_played = False
+        
         
         
 
@@ -218,16 +271,45 @@ class MAIN:
         screen.blit(apple, apple_rect)
 
 
-def draw_game_over_screen(screen):
+    def load_high_score(self):
+        try:
+            with open("high_score.json", "r") as file:
+                data = json.load(file)
+                if isinstance(data, dict):
+                    return data.get("high_score", 0)
+                else:
+                    return 0
+        except FileNotFoundError:
+            return 0
+
+    def save_high_score(self):
+        data = {"high_score": self.high_score}
+        with open("high_score.json", "w") as file:
+            json.dump(data, file)
+
+
+def draw_game_over_screen(screen, high_score):
+    
+    if not main_game.game_over_sound_played:
+        game_over_sound = pygame.mixer.Sound('gameover4.wav')
+        
+        game_over_sound.play()
+        main_game.game_over_sound_played = True
+    
     screen.fill((0, 0, 0))
     font = pygame.font.SysFont('arial', 40)
     title = font.render('Game Over', True, (255, 255, 255))
     restart_button = font.render('R - Restart', True, (255, 255, 255))
     quit_button = font.render('Q - Quit', True, (255, 255, 255))
+    high_score_text = font.render(f'High Score: {high_score}', True, (255, 255, 255))
+    
     screen.blit(title, (screen.get_width() / 2 - title.get_width() / 2, screen.get_height() / 2 - title.get_height() / 3))
     screen.blit(restart_button, (screen.get_width() / 2 - restart_button.get_width() / 2, screen.get_height() / 1.9 + restart_button.get_height()))
     screen.blit(quit_button, (screen.get_width() / 2 - quit_button.get_width() / 2, screen.get_height() / 2 + quit_button.get_height() / 2))
+    screen.blit(high_score_text, (screen.get_width() / 2 - high_score_text.get_width() / 2, screen.get_height() - high_score_text.get_height() - 20))
+    
     pygame.display.update()
+    
 
 def draw_start_menu(screen):
     screen.fill((0, 0, 0))
@@ -240,6 +322,7 @@ def draw_start_menu(screen):
 
 
 pygame.init()
+pygame.mixer.init()
 
 cell_size = 34
 cell_number = 20
@@ -248,9 +331,11 @@ screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 apple = pygame.image.load('apple.png').convert_alpha()
 apple = pygame.transform.scale(apple, (cell_size, cell_size))
+
+bomb = pygame.image.load('bomb.png').convert_alpha()
+bomb = pygame.transform.scale(bomb, (cell_size, cell_size))
+
 game_font = pygame.font.Font(None, 40)
-#game_over_surface = pygame.image.load('g.webp').convert_alpha()
-#game_over_rect = game_over_surface.get_rect(center = (cell_size * cell_number//2, cell_size * cell_number//2))
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 250)
@@ -282,10 +367,14 @@ while True:
                 if main_game.snake.direction.x != -1:
                     main_game.snake.direction = Vector2(1, 0)
 
+        if main_game.snake.direction.x != 0 or main_game.snake.direction.y != 0:
+            main_game.snake.start_moving = True
+
     if main_game.game_state == "start_menu":
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             main_game.game_state = "playing"
+            main_game.snake.start_moving = False
             main_game.is_game_over = False
 
         draw_start_menu(screen)
@@ -297,7 +386,7 @@ while True:
             
 
     elif main_game.game_state == "game_over":
-        draw_game_over_screen(screen)
+        draw_game_over_screen(screen, main_game.high_score)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_r]:
             main_game.game_state = "start_menu"
